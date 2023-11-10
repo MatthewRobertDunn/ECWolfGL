97e41a05ac97b5eed27b77febbe84cf3f14be139
Matthew <mrdunn@gmail.com>
2023-11-10 13:36:08 +1030

    Added back in walls

--- src/OpenGLRenderer.cpp
+++ src/OpenGLRenderer.cpp
@@ -183,8 +183,6 @@ namespace MatGl {
 			walls.insert(walls.end(), wall.begin(), wall.end());
 		}
 
-		/*
-
 		{
 			auto wall = CreateFloor(vec2(x, y), vec4(0.5, 0.5, 0.5, 1.0), -1);
 			walls.insert(walls.end(), wall.begin(), wall.end());
@@ -195,7 +193,6 @@ namespace MatGl {
 			auto wall = CreateCeiling(vec2(x, y), vec4(0.4, 0.4, 0.4, 1.0), -1);
 			walls.insert(walls.end(), wall.begin(), wall.end());
 		}
-		*/
 	}
 
 
